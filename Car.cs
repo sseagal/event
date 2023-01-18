65dda2c6c58d75b8f982af5838dabc212869c099
valera <sseagal12@gmail.com>
2023-01-18 23:59:24 +0200

    one

---
+++ Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+    class Car
+    {
+
+        public delegate void EventDelegate();
+
+        public event EventDelegate TanksIsEmpty;
+        public int FuelLevel { get; set; }
+
+        public void Drive()
+        {
+            if (FuelLevel == 0)
+                TanksIsEmpty();
+            else
+            {
+                FuelLevel -= 15;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Event
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.FuelLevel = 15;
+            car.TanksIsEmpty += () => { Console.WriteLine("Ну а що");};
+
+            #region MyRegion
+            car.Drive();
+            car.Drive();
+            car.Drive();
+            #endregion
+
+        }
+        
+        //static void EventHandler()
+        //{
+        //    Console.WriteLine("Событие пришло");
+        //    Console.WriteLine("Ну вот так");
+        //}
+    }
+}
